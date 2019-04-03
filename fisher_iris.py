1b2d3869550486aa53a7a93da863c1c00295647a
Eoin Dowling <eoindowling505@gmail.com>
2019-04-03 14:23:29 +0100

    Updated code using pandas as calculator and creating dataframes as csv files for easy upload to readme

--- fisher_iris.py
+++ fisher_iris.py
@@ -6,21 +6,14 @@
 import pandas as pd
 import pytablereader as ptr 
 import pytablewriter as ptw
-import csvtomd 
 
 # Reads in the Iris Dataset into python.
 # Code adapted from: https://datatofish.com/use-pandas-to-calculate-stats-from-an-imported-csv-file/
 df = pd.read_csv (r'fisher_iris_dataset.csv')
 
-# Caluclations below adapted from: https://datatofish.com/use-pandas-to-calculate-stats-from-an-imported-csv-file/ 
-# Overall Mean
-sl_overall_mean = round(df['sepal_length'].mean(),3)
-sw_overall_mean = round(df['sepal_width'].mean(),3)
-pl_overall_mean = round(df['petal_length'].mean(),3)
-pw_overall_mean = round(df['petal_width'].mean(),3)
-
-# Mean by Species
-mean_by_species = df.groupby(['species']).mean()
+# Mean by Species code adapted from: https://datatofish.com/use-pandas-to-calculate-stats-from-an-imported-csv-file/
+mean_by_species = round(df.groupby(['species']).mean(),3)
+overall_mean = round(df[['sepal_length','sepal_width','petal_length','petal_width']].mean(),3)
 
 # Overall Median
 sl_overall_median = round(df['sepal_length'].median(),3)
@@ -28,19 +21,11 @@ sw_overall_median = round(df['sepal_width'].median(),3)
 pl_overall_median = round(df['petal_length'].median(),3)
 pw_overall_median = round(df['petal_width'].median(),3)
 
-# Print Results of Calculations
-print ('Overall mean of Sepal Length is around',sl_overall_mean,)
-print ('Overall mean of Sepal Width is around',sw_overall_mean,)
-print ('Overall mean of Petal Length is around',pl_overall_mean,)
-print ('Overall mean of Petal Width is around',pw_overall_mean,)
-
-print ('Overall median of Sepal Length is around',sl_overall_median,)
-print ('Overall median of Sepal Width is around',sw_overall_median,)
-print ('Overall median of Petal Length is around',pl_overall_median,)
-print ('Overall median of Petal Width is around',pw_overall_median,)
-
-# Save data fram generate by panas for mean of species adapted from code in:https://chrisalbon.com/python/data_wrangling/pandas_saving_dataframe_as_csv/
+# Save data generated to csv by panas for mean of species adapted from code in:https://chrisalbon.com/python/data_wrangling/pandas_saving_dataframe_as_csv/
+# Initially attempted to import module to convert csv to .md file however could not get to work: csv2md module
+# Used following website to convert csv to .md: https://donatstudios.com/CsvToMarkdownTable
 mean_by_species.to_csv('mean_by_species.csv')
-csvtomd mean_by_species.csv
+overall_mean.to_csv('overall_mean.csv')
+
 
 
