be68c7dcb3bdf4614d543b1f3f2b7160df764e64
Eoin Dowling <eoindowling505@gmail.com>
2019-04-04 13:16:20 +0100

    Added Seaborn graphs to analyses and histogram Comparison of Sepal Length & Width

--- fisher_iris.py
+++ fisher_iris.py
@@ -4,10 +4,11 @@
 
 # Importing Pandas module to python for calculations and importing of csv file with data.
 # Importing numpy and matplotlib for plots and graphs
+# Importing seaborn to get all species on one graph for stacking
 import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt 
-
+import seaborn as sbn
 
 # Reads in the Iris Dataset into python.
 # Code adapted from: https://datatofish.com/use-pandas-to-calculate-stats-from-an-imported-csv-file/
@@ -29,6 +30,20 @@ overall_mean.to_csv('overall_mean.csv')
 median_by_species.to_csv('median_by_species.csv')
 overall_median.to_csv('overall_median.csv')
 
-df.hist(alpha=0.5, bins=10)
-plt.savefig('Overall_Histogram.png')
+# Seaborn graphs adapting code from: http://python-graph-gallery.com/histogram/
+sbn.distplot(df.sepal_length, color='blue',label='Sepal Length')
+sbn.distplot(df.sepal_width, color='orange',label='Sepal Width')
+plt.legend()
+plt.savefig('sepal_width_sepal_length_comparison.png')
 
+# Code for graphs adapted from: https://pandas.pydata.org/pandas-docs/stable/user_guide/visualization.html 
+df.hist(bins=10)
+plt.savefig('Overall_Histogram.png')
+df.hist(column='sepal_length',stacked=True,bins=10,by='species')
+plt.savefig('sepal_length_Histogram.png')
+df.hist(column='sepal_width',bins=10)
+plt.savefig('sepal_width_Histogram.png')
+df.hist(column='petal_length',bins=10)
+plt.savefig('petal_length_Histogram.png')
+df.hist(column='petal_width',bins=10)
+plt.savefig('petal_width_Histogram.png')
